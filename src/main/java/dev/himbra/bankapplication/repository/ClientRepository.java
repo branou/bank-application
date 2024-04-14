5d80aee91832c4092aa083ffd1014d7818f284b4
himbra <brahimanougmar19@gmail.com>
2024-04-14 18:18:01 +0000

    adding the method findByUserName

--- src/main/java/dev/himbra/bankapplication/repository/ClientRepository.java
+++ src/main/java/dev/himbra/bankapplication/repository/ClientRepository.java
@@ -1,7 +1,11 @@
 package dev.himbra.bankapplication.repository;
 
+import dev.himbra.bankapplication.entity.Account;
 import dev.himbra.bankapplication.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ClientRepository extends JpaRepository<Client,Long> {
+    Optional<Account> findByUserName(String userName);
 }
